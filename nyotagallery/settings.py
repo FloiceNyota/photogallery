0ea39fd099a30de68f68bd44a7c7b3b5f0151718
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-19 12:23:58 -0400

    initial migration and allowed host added

--- nyotagallery/settings.py
+++ nyotagallery/settings.py
@@ -76,7 +76,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS =config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS =['.localhost','.herokuapp.com','127.0.0.1']
 
 
 AUTH_PASSWORD_VALIDATORS = [
