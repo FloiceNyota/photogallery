f8e3f13ef432c6c3a80da4b3a8edf37e60b017f8
FloiceNyota98 <nyotafloice@gmail.com>
2021-10-19 13:59:31 -0400

    category and location looping add

--- gallery/views.py
+++ gallery/views.py
@@ -4,9 +4,13 @@ from .models import *
 def index(request):
   title = 'Nyota Gallery'
   photos = Image.objects.all()
+  categories = Category.objects.all()
+  locations = Location.objects.all()
 
   context={
     'title':title,
     'photos': photos,
+    'categories':categories,
+    'locations': locations
   }
   return render(request, 'index.html', context)
